7ad5953e5b89bfa71103fbfc9ac0a1c451f74cb2
Michal Szwajgier <michalszwajgier@gmail.com>
2022-09-21 21:45:06 +0200

    Add basic structure of owl-carousel

---
+++ js/carousel.js
@@ -0,0 +1,19 @@
+const owl = $(".owl-carousel.gallery-with-projects");
+owl.owlCarousel({
+  loop: true,
+  autoplay: true,
+  items: 3,
+  margin: 10,
+  animateOut: 'fadeOut'
+});
+
+$('.client').on('click', function(event) {
+  const src = $(this).find("img").attr("src")
+  console.log('Img -> ' + src);
+})
+
+owl.on('changed.owl.carousel', function(property) {
+  const current = property.item.index;
+  const src = $(property.target).find(".client").eq(current).find("img").attr('src');
+  console.log('Image current is ' + src);
+});
